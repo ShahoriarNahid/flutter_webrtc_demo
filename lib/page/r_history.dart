263adc480bdf1dd2776c8ccc7bbf03bdca8ac6a1
Md. Shahoriar Nahid <48761690+ShahoriarNahid@users.noreply.github.com>
2023-03-09 11:53:22 +0600

    Update r_history.dart
    feb update

--- lib/page/r_history.dart
+++ lib/page/r_history.dart
@@ -65,7 +65,7 @@ class RHistoryPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('27 Jan 2023 07:00 PM',
+                  Text('09 Jan 2023 07:00 PM',
                       style: TextStyle(color: Colors.black38)),
                   SizedBox(
                     height: 10,
@@ -79,7 +79,163 @@ class RHistoryPage extends StatelessWidget {
                       ),
                       Spacer(),
                       Text(
-                        '৳20',
+                        '৳109',
+                        style: TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: Get.width,
+            height: 70,
+            color: Colors.white,
+            child: Padding(
+              padding: EdgeInsets.only(left: 15, top: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('15 Jan 2023 09:06 PM',
+                      style: TextStyle(color: Colors.black38)),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Recharge Amount',
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      Spacer(),
+                      Text(
+                        '৳250',
+                        style: TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: Get.width,
+            height: 70,
+            color: Colors.white,
+            child: Padding(
+              padding: EdgeInsets.only(left: 15, top: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('07 Feb 2023 08:48 PM',
+                      style: TextStyle(color: Colors.black38)),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Recharge Amount',
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      Spacer(),
+                      Text(
+                        '৳250',
+                        style: TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: Get.width,
+            height: 70,
+            color: Colors.white,
+            child: Padding(
+              padding: EdgeInsets.only(left: 15, top: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('27 Feb 2023 10:03 PM',
+                      style: TextStyle(color: Colors.black38)),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Recharge Amount',
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      Spacer(),
+                      Text(
+                        '৳200',
+                        style: TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: Get.width,
+            height: 70,
+            color: Colors.white,
+            child: Padding(
+              padding: EdgeInsets.only(left: 15, top: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('07 Mar 2023 10:57 AM',
+                      style: TextStyle(color: Colors.black38)),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Recharge Amount',
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      Spacer(),
+                      Text(
+                        '৳41',
                         style: TextStyle(fontWeight: FontWeight.w700),
                       ),
                       SizedBox(
